f80dc089a1d5b6e199552874c3a503f7aa28946a
Attirobert <karobatti@gmail.com>
2023-02-28 14:30:19 +0100

    HüvelykMatyi2 kész

--- HuvelykMatyi2/MainWindow.xaml.cs
+++ HuvelykMatyi2/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace HuvelykMatyi
     public partial class MainWindow : Window
     {
         List<Keszlet> keszlet = new List<Keszlet>();
-        //Dictionary<>
+
         public MainWindow()
         {
             InitializeComponent();
@@ -40,54 +40,67 @@ namespace HuvelykMatyi
         private void keszletLoad()
         {
             Keszlet klt = new Keszlet();
-            klt.nap = btnHetfo;
-            klt.ar = tbxHetfo;
-            keszlet.Add(klt);
 
+            Button[] b = { btnHetfo, btnKedd, btnSzerda, btnCsutortok, btnPentek, btnSzombat, btnVasarnap };
+            TextBox[] t = { tbxHetfo, tbxKedd, tbxSzerda, tbxCsutortok, tbxPentek, tbxSzombat, tbxVasarnap };
 
+            for (int i = 0; i < b.Length; i++)
+            {
+                klt.nap = b[i];
+                klt.ar = t[i];
+                keszlet.Add(klt);
+            }
         }
 
         private void btn_Click(object sender, RoutedEventArgs e)
         {
             if (sender is Button)
             {
-                string nap = (string)(sender as Button).Content;
-                string osszeg = "";
+                string msg = (string)(sender as Button).Content;
 
-                string msg = nap;
-
-                switch (nap)
+                // Megkeresem, hogy melyik gomb lett lenyomva
+                foreach (var klt in keszlet)
                 {
-                    case "Hétfő":
-                        osszeg = tbxHetfo.Text;
+                    if (msg == klt.nap.Content.ToString())
+                    {
+                        msg += ": " + klt.ar.Text + " Ft";
                         break;
-
-                    case "Kedd":
-                        osszeg = tbxKedd.Text;
-                        break;
-
-                    case "Szerda":
-                        osszeg = tbxSzerda.Text;
-                        break;
-
-                    case "Csütörtök":
-                        osszeg = tbxCsutortok.Text;
-                        break;
-
-                    case "Péntek":
-                        osszeg = tbxPentek.Text;
-                        break;
-
-                    case "Szombat":
-                        osszeg = tbxSzombat.Text;
-                        break;
-
-                    case "Vasárnap":
-                        osszeg = tbxVasarnap.Text;
-                        break;
+                    }
                 }
+                //string msg = nap;
 
-                msg += ": " + osszeg + " Ft";
+                //switch (nap)
+                //{
+                //    case "Hétfő":
+                //        osszeg = tbxHetfo.Text;
+                //        break;
+
+                //    case "Kedd":
+                //        osszeg = tbxKedd.Text;
+                //        break;
+
+                //    case "Szerda":
+                //        osszeg = tbxSzerda.Text;
+                //        break;
+
+                //    case "Csütörtök":
+                //        osszeg = tbxCsutortok.Text;
+                //        break;
+
+                //    case "Péntek":
+                //        osszeg = tbxPentek.Text;
+                //        break;
+
+                //    case "Szombat":
+                //        osszeg = tbxSzombat.Text;
+                //        break;
+
+                //    case "Vasárnap":
+                //        osszeg = tbxVasarnap.Text;
+                //        break;
+                //}
+
+                //msg += ": " + osszeg + " Ft";
                 MessageBox.Show(msg);
             }
 
@@ -98,40 +111,50 @@ namespace HuvelykMatyi
             MessageBoxResult valasz = MessageBox.Show("Megváltoztatja az árakat?", "Figyelmeztetés", MessageBoxButton.YesNo, MessageBoxImage.Warning);
 
             if (valasz == MessageBoxResult.Yes)
-            {
                 arAllitas(true);
-            }
         }
 
         private void arAllitas(bool b)
         {
-            tbxHetfo.IsEnabled = b;
-            tbxKedd.IsEnabled = b;
-            tbxSzerda.IsEnabled = b;
-            tbxCsutortok.IsEnabled = b;
-            tbxPentek.IsEnabled = b;
-            tbxSzombat.IsEnabled = b;
-            tbxVasarnap.IsEnabled = b;
+            //tbxHetfo.IsEnabled = b;
+            //tbxKedd.IsEnabled = b;
+            //tbxSzerda.IsEnabled = b;
+            //tbxCsutortok.IsEnabled = b;
+            //tbxPentek.IsEnabled = b;
+            //tbxSzombat.IsEnabled = b;
+            //tbxVasarnap.IsEnabled = b;
+
+            foreach (var klt in keszlet)
+                klt.ar.IsEnabled = b;
         }
 
         private void btnAlkalmaz_Click(object sender, RoutedEventArgs e)
         {
             bool negativ = false;
-            if (int.Parse(tbxHetfo.Text) <= 0) negativ = true;
+            //if (int.Parse(tbxHetfo.Text) <= 0) negativ = true;
 
-            else if (int.Parse(tbxKedd.Text) <= 0) negativ = true;
+            //else if (int.Parse(tbxKedd.Text) <= 0) negativ = true;
 
-            else if (int.Parse(tbxSzerda.Text) <= 0) negativ = true;
+            //else if (int.Parse(tbxSzerda.Text) <= 0) negativ = true;
 
-            else if (int.Parse(tbxCsutortok.Text) <= 0) negativ = true;
+            //else if (int.Parse(tbxCsutortok.Text) <= 0) negativ = true;
 
-            else if (int.Parse(tbxPentek.Text) <= 0) negativ = true;
+            //else if (int.Parse(tbxPentek.Text) <= 0) negativ = true;
 
-            else if (int.Parse(tbxSzombat.Text) <= 0) negativ = true;
+            //else if (int.Parse(tbxSzombat.Text) <= 0) negativ = true;
 
-            else if (int.Parse(tbxVasarnap.Text) <= 0) negativ = true;
+            //else if (int.Parse(tbxVasarnap.Text) <= 0) negativ = true;
+
+            foreach (var klt in keszlet)
+                if (int.Parse(klt.ar.Text) <= 0)
+                {
+                    negativ = true;
+                    break;
+                }
 
             if (!negativ) arAllitas(false);
+
+
         }
 
         private void tbx_LostFocus(object sender, RoutedEventArgs e)
@@ -144,70 +167,84 @@ namespace HuvelykMatyi
 
         private void btnLegolcsobb_Click(object sender, RoutedEventArgs e)
         {
-            // Hétfő
-            string nap = btnHetfo.Content.ToString();
-            int osszeg = int.Parse(tbxHetfo.Text) * nap.Length;
+            string eredmNap = "";
+            int osszeg, eredmOsszeg = int.MaxValue;
 
-            // Kedd
-            string nap2 = btnKedd.Content.ToString();
-            int osszeg2 = int.Parse(tbxKedd.Text) * nap2.Length;
-            if (osszeg > osszeg2)
+            foreach (var klt in keszlet)
             {
-                nap = nap2;
-                osszeg = osszeg2;
+                osszeg = int.Parse(klt.ar.Text) * klt.nap.Content.ToString().Length;
+                if (osszeg < eredmOsszeg)
+                {
+                    eredmOsszeg = osszeg;
+                    eredmNap = klt.nap.Content.ToString();
+                }
             }
 
-            // Szerda
-            nap2 = btnSzerda.Content.ToString();
-            osszeg2 = int.Parse(tbxSzerda.Text) * nap2.Length;
-            if (osszeg > osszeg2)
-            {
-                nap = nap2;
-                osszeg = osszeg2;
-            }
+            //// Hétfő
+            //string nap = btnHetfo.Content.ToString();
+            //int osszeg = int.Parse(tbxHetfo.Text) * nap.Length;
 
-            // Csütörtök
-            nap2 = btnCsutortok.Content.ToString();
-            osszeg2 = int.Parse(tbxCsutortok.Text) * nap2.Length;
-            if (osszeg > osszeg2)
-            {
-                nap = nap2;
-                osszeg = osszeg2;
-            }
+            //// Kedd
+            //string nap2 = btnKedd.Content.ToString();
+            //int osszeg2 = int.Parse(tbxKedd.Text) * nap2.Length;
+            //if (osszeg > osszeg2)
+            //{
+            //    nap = nap2;
+            //    osszeg = osszeg2;
+            //}
 
-            // Péntek
-            nap2 = btnPentek.Content.ToString();
-            osszeg2 = int.Parse(tbxPentek.Text) * nap2.Length;
-            if (osszeg > osszeg2)
-            {
-                nap = nap2;
-                osszeg = osszeg2;
-            }
+            //// Szerda
+            //nap2 = btnSzerda.Content.ToString();
+            //osszeg2 = int.Parse(tbxSzerda.Text) * nap2.Length;
+            //if (osszeg > osszeg2)
+            //{
+            //    nap = nap2;
+            //    osszeg = osszeg2;
+            //}
 
-            // Szombat
-            nap2 = btnSzombat_Copy2.Content.ToString();
-            osszeg2 = int.Parse(tbxSzombat.Text) * nap2.Length;
-            if (osszeg > osszeg2)
-            {
-                nap = nap2;
-                osszeg = osszeg2;
-            }
+            //// Csütörtök
+            //nap2 = btnCsutortok.Content.ToString();
+            //osszeg2 = int.Parse(tbxCsutortok.Text) * nap2.Length;
+            //if (osszeg > osszeg2)
+            //{
+            //    nap = nap2;
+            //    osszeg = osszeg2;
+            //}
 
-            // Vasárnap
-            nap2 = btnVasarnap_Copy2.Content.ToString();
-            osszeg2 = int.Parse(tbxVasarnap.Text) * nap2.Length;
-            if (osszeg > osszeg2)
-            {
-                nap = nap2;
-                osszeg = osszeg2;
-            }
+            //// Péntek
+            //nap2 = btnPentek.Content.ToString();
+            //osszeg2 = int.Parse(tbxPentek.Text) * nap2.Length;
+            //if (osszeg > osszeg2)
+            //{
+            //    nap = nap2;
+            //    osszeg = osszeg2;
+            //}
 
-            MessageBox.Show($"Legolcsóbb, ha {nap}i napon kéred meg Pöttöm Panna kezét, ekkor {osszeg} Ft-ba kerül.");
+            //// Szombat
+            //nap2 = btnSzombat.Content.ToString();
+            //osszeg2 = int.Parse(tbxSzombat.Text) * nap2.Length;
+            //if (osszeg > osszeg2)
+            //{
+            //    nap = nap2;
+            //    osszeg = osszeg2;
+            //}
+
+            //// Vasárnap
+            //nap2 = btnVasarnap.Content.ToString();
+            //osszeg2 = int.Parse(tbxVasarnap.Text) * nap2.Length;
+            //if (osszeg > osszeg2)
+            //{
+            //    nap = nap2;
+            //    osszeg = osszeg2;
+            //}
+
+            MessageBox.Show($"Legolcsóbb, ha {eredmNap}i napon kéred meg Pöttöm Panna kezét, ekkor {eredmOsszeg} Ft-ba kerül.");
         }
 
         private void btnKilepes_Click(object sender, RoutedEventArgs e)
         {
             Close();
         }
+
     }
 }
