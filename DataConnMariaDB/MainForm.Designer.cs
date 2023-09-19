1c88f989af6e09cf1a9b86b0f4a685c5a77eb308
Attirobert <karobatti@gmail.com>
2023-09-19 17:24:14 +0200

    Folytatás a 4. félévben

--- DataConnMariaDB/MainForm.Designer.cs
+++ DataConnMariaDB/MainForm.Designer.cs
@@ -29,33 +29,30 @@ namespace DataConnMariaDB
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.btnData = new System.Windows.Forms.Button();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnData = new System.Windows.Forms.Button();
+            this.dataSet1 = new DataConnMariaDB.DataSet1();
+            this.dataSet1BindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1BindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2});
+            this.dataGridView1.DataSource = this.dataSet1BindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(250, 390);
             this.dataGridView1.TabIndex = 1;
-            // 
-            // btnData
-            // 
-            this.btnData.Location = new System.Drawing.Point(548, 64);
-            this.btnData.Name = "btnData";
-            this.btnData.Size = new System.Drawing.Size(133, 51);
-            this.btnData.TabIndex = 2;
-            this.btnData.Text = "Adatbetöltés";
-            this.btnData.UseVisualStyleBackColor = true;
-            this.btnData.Click += new System.EventHandler(this.btnData_Click);
             // 
             // Column1
             // 
@@ -69,6 +66,26 @@ namespace DataConnMariaDB
             this.Column2.HeaderText = "Jelszó";
             this.Column2.Name = "Column2";
             // 
+            // btnData
+            // 
+            this.btnData.Location = new System.Drawing.Point(548, 64);
+            this.btnData.Name = "btnData";
+            this.btnData.Size = new System.Drawing.Size(133, 51);
+            this.btnData.TabIndex = 2;
+            this.btnData.Text = "Adatbetöltés";
+            this.btnData.UseVisualStyleBackColor = true;
+            this.btnData.Click += new System.EventHandler(this.btnData_Click);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataSet1BindingSource
+            // 
+            this.dataSet1BindingSource.DataSource = this.dataSet1;
+            this.dataSet1BindingSource.Position = 0;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -79,6 +96,8 @@ namespace DataConnMariaDB
             this.Name = "MainForm";
             this.Text = "Adatbázis példa";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1BindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -88,6 +107,8 @@ namespace DataConnMariaDB
         private System.Windows.Forms.Button btnData;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.BindingSource dataSet1BindingSource;
+        private DataSet1 dataSet1;
     }
 }
 
